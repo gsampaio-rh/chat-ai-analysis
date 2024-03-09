d1fd1e1af65adff13255702e2e23c73650406334
Gabriel Sampaio <gsampaio@redhat.com>
2024-03-09 20:02:12 -0300

    locallm files and modules

---
+++ localllm.py
@@ -0,0 +1,159 @@
+import os
+import logging
+import click
+import torch
+
+from modules.qa_pipeline import question_pipeline
+
+from modules.constants import (
+    EMBEDDING_MODEL_NAME,
+    PERSIST_DIRECTORY,
+    MODEL_ID,
+    MODEL_BASENAME,
+    MAX_NEW_TOKENS,
+    MODELS_PATH,
+    CHROMA_SETTINGS
+)
+
+# chose device typ to run on as well as to show source documents.
+@click.command()
+@click.option(
+    "--device_type",
+    default="cuda" if torch.cuda.is_available() else "cpu",
+    type=click.Choice(
+        [
+            "cpu",
+            "cuda",
+            "ipu",
+            "xpu",
+            "mkldnn",
+            "opengl",
+            "opencl",
+            "ideep",
+            "hip",
+            "ve",
+            "fpga",
+            "ort",
+            "xla",
+            "lazy",
+            "vulkan",
+            "mps",
+            "meta",
+            "hpu",
+            "mtia",
+        ],
+    ),
+    help="Device to run on. (Default is cuda)",
+)
+@click.option(
+    "--show_sources",
+    "-s",
+    is_flag=True,
+    help="Show sources along with answers (Default is False)",
+)
+@click.option(
+    "--use_history",
+    "-h",
+    is_flag=True,
+    help="Use history (Default is False)",
+)
+@click.option(
+    "--model_type",
+    default="llama",
+    type=click.Choice(
+        ["llama", "mistral", "non_llama"],
+    ),
+    help="model type, llama, mistral or non_llama",
+)
+@click.option(
+    "--chroma_db_store",
+    is_flag=True,
+    help="Use chromadb (Default is False)",
+)
+@click.option(
+    "--save_qa",
+    is_flag=True,
+    help="whether to save Q&A pairs to a CSV file (Default is False)",
+)
+
+def main(device_type, show_sources, use_history, model_type, chroma_db_store, save_qa):
+    """
+    Implements the main information retrieval task for a localGPT.
+
+    This function sets up the QA system by loading the necessary embeddings, vectorstore, and LLM model.
+    It then enters an interactive loop where the user can input queries and receive answers. Optionally,
+    the source documents used to derive the answers can also be displayed.
+
+    Parameters:
+    - device_type (str): Specifies the type of device where the model will run, e.g., 'cpu', 'mps', 'cuda', etc.
+    - show_sources (bool): Flag to determine whether to display the source documents used for answering.
+    - use_history (bool): Flag to determine whether to use chat history or not.
+
+    Notes:
+    - Logging information includes the device type, whether source documents are displayed, and the use of history.
+    - If the models directory does not exist, it creates a new one to store models.
+    - The user can exit the interactive loop by entering "exit".
+    - The source documents are displayed if the show_sources flag is set to True.
+
+    """
+
+    logging.info(f"Running on: {device_type}")
+    logging.info(f"Display Source Documents set to: {show_sources}")
+    logging.info(f"ChromaDB set to: {chroma_db_store}")
+    logging.info(f"Use history set to: {use_history}")
+
+    # check if models directory do not exist, create a new one and store models here.
+    if not os.path.exists(MODELS_PATH):
+        os.mkdir(MODELS_PATH)
+
+    query = "What is electroencephalography?"
+
+    qa = question_pipeline(
+        device_type, use_history, query
+    )
+
+    answer = qa
+    
+    # Get the answer from the chain
+    # res = qa(query)
+    # answer = res["result"]
+
+    # Print the result
+    print("\n\n> Question:")
+    print(query)
+    print("\n> Answer:")
+    print(answer)
+
+    # Interactive questions and answers
+    # while True:
+    #     query = input("\nEnter a query: ")
+    #     if query == "exit":
+    #         break
+    #     # Get the answer from the chain
+    #     res = qa(query)
+    #     answer, docs = res["result"], res["source_documents"]
+
+    #     # Print the result
+    #     print("\n\n> Question:")
+    #     print(query)
+    #     print("\n> Answer:")
+    #     print(answer)
+
+    #     if show_sources:  # this is a flag that you can set to disable showing answers.
+    #         # # Print the relevant sources used for the answer
+    #         print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    #         for document in docs:
+    #             print("\n> " + document.metadata["source"] + ":")
+    #             print(document.page_content)
+    #         print("----------------------------------SOURCE DOCUMENTS---------------------------")
+
+    #     # Log the Q&A to CSV only if save_qa is True
+    #     if save_qa:
+    #         utils.log_to_csv(query, answer)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
+    )
+    main()
